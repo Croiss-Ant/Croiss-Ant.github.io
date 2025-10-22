0fadc66670e4375b3e9b96c67f277639cb8eafb2
antoine <antoinebellion79@gmail.com>
2025-10-22 17:59:35 +0200

    feat: ground work for the linktree + test config.yml

---
+++ js/links.js
@@ -0,0 +1,31 @@
+export var linksMain = [
+    {
+        name: "YouTube",
+        link: "https://www.youtube.com/@Croiss-Ant",
+        image: "fa-brands fa-youtube"
+    }, {
+        name: "Github",
+        link: "https://www.youtube.https://github.com/Croiss-Ant/@Croiss-Ant",
+        image: "fa-brands fa-github"
+    }
+]
+
+export const linksPro = [
+    {
+        name: "Linkedin",
+        link: "https://www.linkedin.com/in/bellionantoine/",
+        image: "fa-brands fa-linkedin",
+        color: "#0A66C2"
+    },
+    {
+        name: "Mail",
+        link: "mailto:antoinebellion79@gmail.com",
+        image: "fa-brands fa-font-awesome"
+    }
+]
+
+//All -> Youtube, Github
+
+//Online -> Deviantart(archive), Discord, Steam 
+
+//Pro -> Linkedin, Gmail
---
+++ js/main.js
@@ -0,0 +1,37 @@
+import { linksMain, linksPro } from "./links.js";
+
+const linkDiv = document.getElementById("links");
+
+function addLink(name, link, image, color) {
+    if (!color) {
+        return `
+        <a href="${link}" class="link ${name.toLowerCase()}" target="blank">
+            <i class="${image}"></i>
+            <span>${name} </span>
+            <i class="fa-solid fa-paperclip" onclick="copyLink(${link})"></i>
+        </a>`
+    } else {
+        return `
+        <a href="${link}" class="link ${name.toLowerCase()}" target="blank">
+            <i class="${image}" style="color: ${color};"></i>
+            <span>${name} </span>
+        </a>`
+    }
+}
+
+function copyLink(link) {
+    navigator.clipboard.writeText(link)
+}
+
+let allLinks = "";
+
+linksMain.forEach((l) => {
+    let link = l.link
+    let name = l.name
+    let image = l.image
+    let color = l.color
+
+    allLinks += addLink(name, link, image, color)
+});
+
+linkDiv.innerHTML = allLinks;
