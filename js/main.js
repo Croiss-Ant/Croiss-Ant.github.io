54c2f89b78bd577d68d306f535f803ff3f4c391d
antoine <antoinebellion79@gmail.com>
2025-10-26 15:57:14 +0100

    feat: early footer work
    commented to avoid issues, will work on it later

--- js/main.js
+++ js/main.js
@@ -16,11 +16,11 @@ function writeDiv(image, name, about) {
 }
 
 if (display == "pro") {
-    writeDiv("https://image2url.com/images/1761484200064-2c3e6b91-e9d1-4630-b801-9e43cc4ca326.jpg", "", "test")
+    writeDiv("https://image2url.com/images/1761484200064-2c3e6b91-e9d1-4630-b801-9e43cc4ca326.jpg", "Antoine Bellion", "")
     links = linksMain.concat(linksPro)
 
 } else {
-    writeDiv("https://image2url.com/images/1761484172085-be4b7a67-34c0-4c2c-8c2d-98f752a84bb0.png", "Ant", "test")
+    writeDiv("https://image2url.com/images/1761484172085-be4b7a67-34c0-4c2c-8c2d-98f752a84bb0.png", "Ant", "")
     links = linksMain.concat(linksOnline)
 }
 
