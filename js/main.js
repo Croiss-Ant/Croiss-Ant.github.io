3ee0cbf6ea1f1e63e6bd703e2bc94dd5fd687bd5
antoine <antoinebellion79@gmail.com>
2025-10-26 16:33:04 +0100

    feat: 404 rewriting thanks to https://github.com/h-kuwayama/ghp-rewrite

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
 import { linksMain, linksOnline, linksPro } from "./links.js";
 
+// https://github.com/h-kuwayama/ghp-rewrite
+/*
+* Config
+*/
+let file_name = '';
+let parameter_key = 'display';
+    
+/*
+* Env
+*/
+let path_name = window.location.pathname;
+let query_string = window.location.search;
+let base_dir = path_name.split(file_name)[0];
+    
+/*
+* Process
+* (Rewrite URL using historyAPI)
+*/
+let parameter
+let parameter_value
+let match_condition = new RegExp(parameter_key + '=[a-z0-9-_]+$');
+if (parameter = query_string.match(match_condition)) {
+    parameter_value = parameter[0].split('=')[1];
+    history.replaceState(null, null, base_dir + file_name.replace('.html', '/') + parameter_value);
+}
+
 let links = ""
 
-const currentUrl = new URL(window.location)
-const display = currentUrl.searchParams.get("display")
+//const currentUrl = new URL(window.location)
+//const display = currentUrl.searchParams.get("display")
 
 let profileDiv = document.getElementById("profile")
 let nameDiv = document.getElementById("name")
@@ -15,7 +41,7 @@ function writeDiv(image, name, about) {
     aboutDiv.innerHTML = `<div>${about}</div>`
 }
 
-if (display == "pro") {
+if (parameter_value == "pro") {
     writeDiv("https://image2url.com/images/1761484200064-2c3e6b91-e9d1-4630-b801-9e43cc4ca326.jpg", "Antoine Bellion", "")
     links = linksMain.concat(linksPro)
 
