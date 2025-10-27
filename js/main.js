955b0f690c903927a8a865a79576ef680890b174
antoine <antoinebellion79@gmail.com>
2025-10-27 13:58:07 +0100

    feat: dynamic title & favicon

--- js/main.js
+++ js/main.js
@@ -9,11 +9,13 @@ let profileDiv = document.getElementById("profile")
 let nameDiv = document.getElementById("name")
 let aboutDiv = document.getElementById("about")
 
+let favicon = document.querySelector(`link[rel~="icon"]`)
+
 function writeDiv(image, name, about, artistId = null, artLink = null) {
     nameDiv.innerHTML = `<div>${name}</div>`
     aboutDiv.innerHTML = `<div>${about}</div>`
     if (artistId) {
-        profileDiv.innerHTML = `<a href="${artLink}" target="blank" title="Original art by ${artistId} | Click to check them out"><img class="thumbnail" src="${image}" alt="thumbnail"></a>`
+        profileDiv.innerHTML = `<a href="${artLink}" target="blank" title="Original art by ${artistId} | Click to check them out !"><img class="thumbnail" src="${image}" alt="thumbnail"></a>`
     } else {
         profileDiv.innerHTML = `<img class="thumbnail" src="${image}" alt="thumbnail">`
     }
@@ -22,10 +24,14 @@ function writeDiv(image, name, about, artistId = null, artLink = null) {
 if (display == "pro") {
     writeDiv(bioPro.image, bioPro.name, bioPro.about)
     links = linksMain.concat(linksPro)
+    document.title = `${bioPro.name} | Liens utiles`
+    favicon.href = bioPro.image
 
 } else {
     writeDiv(bioMain.image, bioMain.name, bioMain.about, bioMain.artistId, bioMain.artLink)
     links = linksMain.concat(linksOnline)
+    document.title = `${bioMain.name} | Links`
+    favicon.href = bioMain.image
 }
 
 const linksDiv = document.getElementById("links")
