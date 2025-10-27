820233cecc94d8d72cafaae1ddbab6a5fb98c964
antoine <antoinebellion79@gmail.com>
2025-10-27 14:42:30 +0100

    docs: main code documentation

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 import { linksMain, linksOnline, linksPro, bioMain, bioPro } from "./data.js";
 
+/* ---------- CONFIG ---------- */
+/* ---- link containers ---- */
 let links = ""
+let addLinks = ""
 
+/* ---- http GET parameter ---- */
 const currentUrl = new URL(window.location)
 const display = currentUrl.searchParams.get("q")
 
-let profileDiv = document.getElementById("profile")
-let nameDiv = document.getElementById("name")
-let aboutDiv = document.getElementById("about")
+/* -- html elements -- */
+const favicon = document.querySelector(`link[rel~="icon"]`)
+const profileDiv = document.getElementById("profile")
+const nameDiv = document.getElementById("name")
+const aboutDiv = document.getElementById("about")
+const linksDiv = document.getElementById("links")
 
-let favicon = document.querySelector(`link[rel~="icon"]`)
-
+/* ---------- FUNCTIONS ---------- */
+/* ---- rewrite bio elements ---- */
 function writeDiv(image, name, about, artistId = null, artLink = null) {
     nameDiv.innerHTML = `<div>${name}</div>`
     aboutDiv.innerHTML = `<div>${about}</div>`
@@ -21,21 +28,7 @@ function writeDiv(image, name, about, artistId = null, artLink = null) {
     }
 }
 
-if (display == "pro") {
-    writeDiv(bioPro.image, bioPro.name, bioPro.about)
-    links = linksMain.concat(linksPro)
-    document.title = `${bioPro.name} | Liens utiles`
-    favicon.href = bioPro.image
-
-} else {
-    writeDiv(bioMain.image, bioMain.name, bioMain.about, bioMain.artistId, bioMain.artLink)
-    links = linksMain.concat(linksOnline)
-    document.title = `${bioMain.name} | Links`
-    favicon.href = bioMain.image
-}
-
-const linksDiv = document.getElementById("links")
-
+/* ---- add a new link element ---- */
 function addLink(id, name, link, icon) {
     return `
     <a href="${link}" class="link ${id}" target="blank">
@@ -45,8 +38,22 @@ function addLink(id, name, link, icon) {
     </a>`
 }
 
-let addLinks = ""
+/* ---------- MAIN ---------- */
+/* ---- dynamically update page content ---- */
+if (display == "pro") {
+    document.title = `${bioPro.name} | Liens utiles`
+    favicon.href = bioPro.image
+    writeDiv(bioPro.image, bioPro.name, bioPro.about)
+    links = linksMain.concat(linksPro)
 
+} else {
+    writeDiv(bioMain.image, bioMain.name, bioMain.about, bioMain.artistId, bioMain.artLink)
+    links = linksMain.concat(linksOnline)
+    document.title = `${bioMain.name} | Links`
+    favicon.href = bioMain.image
+}
+
+/* ---- dynamically create all the necessary links.. ---- */
 links.forEach((l) => {
     let id = l.id
     let link = l.link
@@ -56,4 +63,5 @@ links.forEach((l) => {
     addLinks += addLink(id, name, link, icon)
 })
 
+/* ---- ..and inject them into the page ---- */
 linksDiv.innerHTML = addLinks
