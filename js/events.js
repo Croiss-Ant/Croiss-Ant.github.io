ed2890a125a34ce54e02ded3d12c599a17f8e8bf
antoine <antoinebellion79@gmail.com>
2025-11-05 16:53:57 +0100

    test: popup logic

--- js/events.js
+++ js/events.js
@@ -1,10 +1,25 @@
+const wrapper = document.getElementById("wrapper")
+const popup = document.getElementById("popup")
 const bug = document.getElementById("bug")
+const audio = document.getElementById("audio")
 
-bug.addEventListener("click", () => {
-    playBug()
-})
+function popUp(header, text, image) {
+    wrapper.classList.add("inactive")
+    popup.classList.remove("inactive")
+}
 
 function playBug() {
-    let audio = document.getElementById("audio")
     audio.play()
 }
+
+bug.addEventListener("click", () => {
+    if (popup.classList.contains("inactive")) {
+        popUp();
+        playBug();
+    }
+})
+
+audio.addEventListener("ended", () => {
+    wrapper.classList.remove("inactive")
+    popup.classList.add("inactive")
+})
