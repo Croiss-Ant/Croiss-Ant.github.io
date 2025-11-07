c3366cba3e3a2ee138eb2c311b45507c20c1d8c1
antoine <antoinebellion79@gmail.com>
2025-11-07 12:42:41 +0100

    feat: popup bug :3

--- js/events.js
+++ js/events.js
@@ -1,9 +1,16 @@
 const wrapper = document.getElementById("wrapper")
 const popup = document.getElementById("popup")
+const popupImg = document.getElementById("popupImg")
+const popupTitle = document.getElementById("popupTitle")
+const popupText = document.getElementById("popupText")
+
 const bug = document.getElementById("bug")
 const audio = document.getElementById("audio")
 
-function popUp(header, text, image) {
+function popUp(header, text, image, link = null) {
+    popupTitle.innerHTML = `<div>${header}</div>`
+    popupText.innerHTML = `<div>${text}</div>`
+    popupImg.innerHTML = `<img class="img" src="${image}"></img>`
     wrapper.classList.add("inactive")
     popup.classList.remove("inactive")
 }
@@ -14,8 +21,15 @@ function playBug() {
 
 bug.addEventListener("click", () => {
     if (popup.classList.contains("inactive")) {
-        popUp();
+        popUp("Get stick bugged lol", "You found a secret, I guess ?", "https://media1.tenor.com/m/P-bmt667a_oAAAAd/stick-bug-stick-bugged.gif");
         playBug();
+    }
+})
+
+popup.addEventListener("click", () => {
+    if (audio.paused) {
+        wrapper.classList.remove("inactive")
+        popup.classList.add("inactive")
     }
 })
 
