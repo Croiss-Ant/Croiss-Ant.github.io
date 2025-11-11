740fb1f0b3b69df9ffda2a8a074642b9ea0f888f
antoine <antoinebellion79@gmail.com>
2025-11-11 15:36:31 +0100

    fix/test: video & image rewriting the other

--- js/events.js
+++ js/events.js
@@ -8,11 +8,15 @@ const bug = document.getElementById("bug")
 const audio = document.getElementById("audio")
 const video = document.getElementById("video")
 
-function popUp(header, text, image = null, link = null) {
+const test = document.getElementById("test")
+
+function popUp(header, text, image = null) {
+    popupImg.innerHTML = ""
     popupTitle.innerHTML = `<div>${header}</div>`
     popupText.innerHTML = `<div>${text}</div>`
     if (image) {
         popupImg.innerHTML = `<img class="img" src="${image}"></img>`
+        video.classList.add("inactive")
     }
     wrapper.classList.add("inactive")
     popup.classList.remove("inactive")
@@ -25,9 +29,14 @@ function playBug() {
 
 bug.addEventListener("click", () => {
     if (popup.classList.contains("inactive")) {
+        video.classList.remove("inactive")
         popUp("Get stick bugged lol", "You found a secret, I guess ?");
         playBug();
     }
+})
+
+test.addEventListener("click", () => {
+    popUp("What have you done", "...", "./assets/test.jpg")
 })
 
 popup.addEventListener("click", () => {
